73683f1434c89c160d11c1122c769fa41ea07005
ins-alex <urcn2@mail.ru>
2024-08-12 14:47:07 +0200

    comment out test

--- tests/test_security.py
+++ tests/test_security.py
@@ -2,6 +2,7 @@ import requests
 
 
 def test_request_limit():
+    print("OK")
     # query = "SELECT * WHERE {{ ?s ?p ?o }}"
     # url = f"http://141.57.8.18:8099/explanation?query_text={query}&language=en&shots=0&model=gpt-3.5-turbo"
     # r = requests.get(url)
